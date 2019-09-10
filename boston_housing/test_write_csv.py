3f92ba77de61f6e1e82080fb1f947cc11d0c006a
LuposX <36456825+LuposX@users.noreply.github.com>
2019-09-10 11:26:10 +0200

    Delete test_write_csv.py

--- boston_housing/test_write_csv.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-row = [["weight_bias"], [2], [2]]
-filename = "linear_regression_housing_weights.csv"
-with open(filename, "w+", newline='') as writeFile:
-    writer = csv.writer(writeFile)
-    writer.writerows(row)
-
-writeFile.close()
