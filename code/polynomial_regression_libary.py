d2fab51c7e3cbcad1b97c19c33644747691e30e7
LuposX <36456825+LuposX@users.noreply.github.com>
2019-09-21 21:51:57 +0200

    removed unused imports

--- code/polynomial_regression_libary.py
+++ code/polynomial_regression_libary.py
@@ -1,13 +1,6 @@
 import time
 from misc_libary import loss
 import sys
-import csv
-import seaborn as sns
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from mpl_toolkits import mplot3d
-import pandas as pd
-import numpy as np
 
 class PolynomialRegression:
     def __init__(self, df, args):
